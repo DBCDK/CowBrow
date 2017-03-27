03205301700a2f469ee5432707ab4089f51aa2ac
jsj <jsj@b87a6194-a221-0410-ad6f-3b8ec35bba13>
2017-03-27 13:48:37 +0000

    CowBrow: exit when session creation failed
    git-svn-id: https://svn.dbc.dk/repos/skuffe/jsj@111729 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/jens/mq/Main.java
+++ src/main/java/dk/jens/mq/Main.java
@@ -1,9 +1,14 @@
 package dk.jens.mq;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.jms.Session;
 import java.util.List;
 
 public class Main {
+    private static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args)
     {
         ArgsHandler.Args parsedArgs = ArgsHandler.handleArgs(args);
@@ -12,6 +17,10 @@ public class Main {
         Session session = cowBrow.connect(parsedArgs.getString("host"),
             parsedArgs.getString("port"), parsedArgs.getString("user"),
             parsedArgs.getString("password"));
+        if(session == null) {
+            LOGGER.error("starting jms session failed");
+            System.exit(1);
+        }
         List<String> actions = parsedArgs.getList("action");
         if(actions.get(0).equals("cli")) {
             Cli cli = new Cli(cowBrow);
